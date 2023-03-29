a77ae650d86c4851109014e0d44befec765ec997
eniac555 <1150304664@qq.com>
2023-03-29 16:14:13 +0800

    first commit

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -8,13 +8,18 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -37,7 +42,47 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private RedisIdWorker redisIdWorker;
 
-    //秒杀优惠券
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    //秒杀优惠券 ----基础版
+
+//    @Override
+//    public Result seckillVoucher(Long voucherId) {
+//        //1.查询优惠券信息
+//        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+//        //2.判断秒杀是否开始
+//        if (LocalDateTime.now().isBefore(voucher.getBeginTime())) {
+//            //尚未开始
+//            return Result.fail("秒杀尚未开始");
+//        }
+//        //3.判断秒杀是否结束
+//        if (LocalDateTime.now().isAfter(voucher.getEndTime())) {
+//            //已经结束
+//            return Result.fail("秒杀已经结束");
+//        }
+//        //4.判断库存是否充足
+//        if (voucher.getStock() < 1) {
+//            //库存不足
+//            return Result.fail("库存不足");
+//        }
+//        Long userId = UserHolder.getUser().getId();
+//
+//        //=======释放锁的时机和事务处理之间的复杂联系=======
+//        //集群模式下锁不住诶，多个服务时每个服务都会有一个服务内可用的锁
+//        synchronized (userId.toString().intern()) {
+//            //获取代理对象（和事务有关）
+//            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+//            //加了悲观锁，锁是userId
+//            return proxy.createVoucherOrder(voucherId);
+//        }
+//    }
+
+
+    //秒杀优惠券 ----分布式锁--基础版的分布式锁
     @Override
     public Result seckillVoucher(Long voucherId) {
         //1.查询优惠券信息
@@ -58,18 +103,35 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足");
         }
         Long userId = UserHolder.getUser().getId();
+        //创建锁对象
+        //SimpleRedisLock lock = new SimpleRedisLock(stringRedisTemplate, "order:" + userId);
 
-        //=======释放锁的时机和事务处理之间的复杂联系=======
-        //集群模式下锁不住诶，多个服务时每个服务都会有一个服务内可用的锁
-        synchronized (userId.toString().intern()) {
+        // todo
+        RLock lock = redissonClient.getLock("order:" + userId);//使用redisson创建锁对象
+        boolean isLock = lock.tryLock();
+
+        //获取锁
+        //boolean isLock = lock.tryLock(5);
+        //判断获取锁是否 成功
+        if (!isLock) {
+            //失败，返回错误信息或者重试，这里应该直接失败
+            return Result.fail("一人仅允许下一单");
+        }
+        //获取锁成功
+        try {
             //获取代理对象（和事务有关）
             IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
             //加了悲观锁，锁是userId
             return proxy.createVoucherOrder(voucherId);
+        } finally {
+            //lock.unLock();
+            lock.unlock();
         }
+
     }
 
 
+    //判断是否一人一单并创建订单
     @Transactional//有订单表和优惠券表的共同操作，最好加上事务控制
     public Result createVoucherOrder(Long voucherId) {
         //5.一人一单判断
