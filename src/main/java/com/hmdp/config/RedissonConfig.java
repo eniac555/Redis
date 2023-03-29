5989f694c2256ac9e9b6ef4e5018132d51f4fecd
eniac555 <1150304664@qq.com>
2023-03-29 21:38:39 +0800

    first commit

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -17,4 +17,22 @@ public class RedissonConfig {
         //创建对象
         return Redisson.create(config);
     }
+
+    @Bean
+    public RedissonClient redissonClient2(){
+        //配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://192.168.163.128:6380");
+        //创建对象
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public RedissonClient redissonClient3(){
+        //配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://192.168.163.128:6381");
+        //创建对象
+        return Redisson.create(config);
+    }
 }
